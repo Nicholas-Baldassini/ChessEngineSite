46a2ab5770d2f6550e1239f15b37b4d3d2324ba3
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2021-10-24 16:12:59 -0400

    FIXED

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ function App() {
         <ThreeComponent />
       </div>
       <div id='NonThreeGraphics' style={{ position: "absolute", left: "0px", top: "0px" }}>
-        <h1 style={{ color: "white" }}>This wont work</h1>
-        <p style={{ color: "white" }}>Hello world</p>
-        <p style={{ color: "white" }}>JUST WORK ALREADY</p>
-        <p style={{ color: "white" }}>OLAY OKAY OLAY</p>
       </div>
     </div>
   );
