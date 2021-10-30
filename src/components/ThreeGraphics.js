55c5d1614d6f04055d0d06eca0a9f34f55613762
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2021-10-30 17:20:12 -0400

    Added spinning cubes in background

--- src/components/ThreeGraphics.js
+++ src/components/ThreeGraphics.js
@@ -1,22 +1,49 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 
 function ThreeGraphics(renderer){
 
         var scene = new THREE.Scene();
-        scene.background = new THREE.Color("rgb(100, 100, 100)");
+        scene.background = new THREE.Color("rgb(50, 50, 50)");
         var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        var cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+        const cubeWidth = 1.5
+        var boxGeometry = new THREE.BoxGeometry(cubeWidth, cubeWidth, cubeWidth);
+         var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+       const controls = new OrbitControls(camera, renderer.domElement);
+
+         
+
+
+        let cubes = []
+        function createCubes (){
+          const cube = new THREE.Mesh(boxGeometry, material);
+          var [x, y, z] = Array(3)
+            .fill()
+            .map(() => THREE.MathUtils.randFloatSpread(200));
+
+            cube.position.set(x, y, z);
+            cubes.push(cube);
+            scene.add(cube)
+        }
+        for (let i=0; i<500; i++){
+          createCubes();
+        }
+
+
+
         camera.position.z = 5;
         var animate = function () {
           requestAnimationFrame(animate);
-          cube.rotation.x += 0.09;
-          cube.rotation.y += 0.09;
-          cube.rotation.z += 0.01;
+          for(let i=0; i<cubes.length; i++){
+            cubes[i].rotation.x += 0.09;
+            cubes[i].rotation.y += 0.09;
+            cubes[i].rotation.z += 0.01;
+          }
+
           renderer.render(scene, camera);
         };
+        controls.update();
         animate();
 }
 
