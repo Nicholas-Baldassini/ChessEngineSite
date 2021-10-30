df4d97e8565047f349dabdde48e59786eb0a07d8
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2021-10-30 15:56:56 -0400

    Fixed game storage problem, now each game is stored on client side

--- build/board.js
+++ build/board.js
@@ -1,49 +1,17 @@
 /* eslint-disable no-undef */
-
-//let humanColour = Math.round(Math.random()) ? "white": "black"
-
 const local = "http://localhost:5000/";
-const resetLocal = "http://localhost:5000/reset";
+const getMoveLocal = "http://localhost:5000/getMove";
 
 const live = "https://chess-engine-backend.herokuapp.com/";
-const resetLive = "https://chess-engine-backend.herokuapp.com/reset";
+const getMoveLive = "https://chess-engine-backend.herokuapp.com/getMove";
 
-const localURLS = {home: local, reset: resetLocal}
-const liveURLS = {home: live, reset: resetLive}
+const localURLS = { home: local, getMove: getMoveLocal };
+const liveURLS = { home: live, getMove: getMoveLive };
 
-const URLS = [localURLS, liveURLS][1]
+const URLS = [localURLS, liveURLS][0]; //0-local, 1-live
 
-
-
-// let turn = ["white", "black"];
-// let currentTurn = turn[0];
-
-function onDragStart(source, piece, position, orientation) {
-  // do not pick up pieces if the game is over
-  // only pick up pieces for White
-  if (piece.search(/^b/) !== -1) return false;
-}
-
-function onDrop(source, target) {
-  // see if the move is legal
-  //console.log("Droped");
-  const moveObj = {
-    from: source,
-    to: target,
-    promotion: "q", // NOTE: always promote to a queen for example simplicity
-  };
-  PostFEN(moveObj);
-  //var move = game.move()
-
-  // illegal move
-  //if (move === null) return 'snapback'
-
-  // make random legal move for black
-  //window.setTimeout(makeRandomMove, 250)
-}
-
-async function PostFEN(pos) {
-  const data = { key: pos };
+async function getMove(gameParam) {
+  const data = { gameKey: gameParam };
   const options = {
     method: "POST",
     body: JSON.stringify(data),
@@ -51,40 +19,78 @@ async function PostFEN(pos) {
       "Content-Type": "application/json",
     },
   };
-
-  fetch(URLS.home, options)
+  // GET on .../
+  fetch(URLS.getMove, options)
     .then((response) => response.json())
-    .then((data) => {
-      //console.log("Success:", data.ASC);
-      board1.position(data.ASC);
+    .then((response) => {
+      game.load_pgn(response.PGN);
+      board.position(game.fen());
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.error("Fetch Error:", error);
     });
 }
 
-var board1 = Chessboard("board1", {
-  draggable: true,
-  dropOffBoard: "snapback",
-  sparePieces: true,
-  showNotation: true,
-  onDrop: onDrop,
-  onDragStart: onDragStart,
-});
+var board = null;
+var game = new Chess();
 
-document.getElementById("startBtn").addEventListener("click", () => {
-  board1.start();
-  ResetBoard();
-});
-document.getElementById("clearBtn").addEventListener("click", () => board1.clear());
+function onDragStart(source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false;
 
-async function ResetBoard() {
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  fetch(URLS.reset, options);
+  // only pick up pieces for White
+  if (piece.search(/^b/) !== -1) return false;
 }
+
+function makeRandomMove() {
+  var possibleMoves = game.moves();
+
+  // game over
+  if (possibleMoves.length === 0) return;
+
+  var randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  game.move(possibleMoves[randomIdx]);
+  board.position(game.fen());
+}
+
+function onDrop(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: "q", // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return "snapback";
+
+  // make random legal move for black
+  //window.setTimeout(makeRandomMove, 250);
+
+  getMove(game.pgn());
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+var config = {
+  draggable: true,
+  position: "start",
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+};
+board = Chessboard("board1", config);
+
+// Buttons
+document.getElementById("startBtn").addEventListener("click", () => {
+  board.start();
+  game.reset();
+});
+document.getElementById("clearBtn").addEventListener("click", () => {
+  board.clear();
+  game.reset();
+});
