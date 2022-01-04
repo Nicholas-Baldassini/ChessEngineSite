a323a6c4b9a5a7d0661aba6f3811c7c5e41cceb2
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2022-01-04 18:49:29 -0500

    Added minimax algorithm unfinished static eval

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import ThreeComponent from "./components/ThreeComponent";
 
-
-
 function App() {
   return (
     <div className='App'>
-      <div id='ThreeGraphics'><ThreeComponent /></div>
+      <div id='ThreeGraphics'>
+        <ThreeComponent />
+      </div>
       <div id='NonThreeGraphics' style={{ position: "absolute", left: "0px", top: "0px" }}>
-        {/* <h1>Hello</h1> */}
+        <h1>
+          Alert:
+          <span className='badge badge-warning'>UNDER REPAIRS</span>
+        </h1>
       </div>
     </div>
   );
